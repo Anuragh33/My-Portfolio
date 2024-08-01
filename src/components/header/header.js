b23dc406893bc5d139cd6a08a66b9b6ede9853d7
Ragidimilli Anuragh <Princeanuragh1@gmail.com>
2024-08-01 12:57:43 -0500

    Update header.js

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -17,12 +17,12 @@ const Header = () => {
       </RelativeContainer>
 
       <BasicText>
-        I am a highly skilled and versatile full-stack developer with 4 plus
-        years of experience in designing, developing, and maintaining web
-        applications. Proficient in both front-end and back-end technologies, I
-        have a deep understanding of the full software development lifecycle,
-        from initial planning and design to deployment and maintenance....Here's
-        my current tech stack:
+        I am a skilled and versatile <strong>Full-Stack Developer</strong> with
+        <strong>4 plus years</strong> of experience in designing, developing,
+        and maintaining web applications. Proficient in both front-end and
+        back-end technologies, I have a deep understanding of the full software
+        development lifecycle, from initial planning and design to deployment
+        and maintenance....Here's my current tech stack:
       </BasicText>
       <Skills />
     </HeaderContainer>
