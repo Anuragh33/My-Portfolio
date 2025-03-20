f813f54ce507ef4b4454adedac75a5548aabe09a
Anuragh Ragidimilli <n374r654@wichita.edu>
2025-03-20 07:49:17 -0500

    Update header.js

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -18,10 +18,11 @@ const Header = () => {
       <BasicText>
         {' '}
         Experienced Full Stack Developer passionate about building scalable and
-        reusable user interfaces using Next, React, and Typescript. Focused on
-        developing secure, high-performance web applications, optimizing
-        functionality, and integrating new features while working with modern
-        architecture patterns, in an Agile environment. Here's my tech stack,
+        reusable user interfaces using <strong>Next JS</strong>,
+        <strong>React JS</strong>, and <strong>Typescript.</strong>.<br></br>
+        Beyond frontend development, I have strong backend expertise in Node JS,
+        Django, Drizzle ORM, GraphQL and RESTful APIs, along with proficiency in
+        SDLC and Git. Here's my tech stack,
       </BasicText>
 
       <Skills />
