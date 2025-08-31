0935e7d7e35bdf7af0fce14616b026f5882f46ad
Anuragh <princeanuragh1@gmail.com>
2025-08-30 19:58:28 -0500

    Update header.js

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -19,7 +19,8 @@ const Header = () => {
         {' '}
         Experienced Full Stack Developer passionate about building scalable and
         reusable user interfaces using <strong>Next JS</strong>,
-        <strong>React JS</strong>, and <strong>Typescript.</strong>.<br></br>
+        <strong> React JS</strong>, and <strong>Typescript.</strong>
+        <br></br>
         Beyond frontend development, I have strong backend expertise in Node JS,
         Django, Drizzle ORM, GraphQL and RESTful APIs, along with proficiency in
         SDLC and Git. Here's my tech stack,
